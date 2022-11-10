98ab0d62cd00d254e9a11a46844c09fb8ab5b078
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-11-10 19:01:45 -0300

    Se crean funciones para el consumo de API

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,42 @@ const urlDev = 'http://localhost:4000/api/v1';
 /**
  * Funciones de consulta a la API
  */
+/**
+ * Funcion para obtener todos los productos
+ */
+const getProducts = async() => {
+    const res = await fetch(`${urlDev}/products`);
+    const data = await res.json();
+    if(res.status === 404){
+        console.log(data.message);
+        return;
+    };
+    return data;
+};
+
+/**
+ * Funcion para obtener todas las categorias
+ */
+const getCategories = async() => {
+    const res = await fetch(`${urlDev}/category`);
+    const data = await res.json();
+    if(res.status === 404){
+        console.log(data.message);
+        return;
+    };
+    return data;
+};
+
+/** 
+ * Funcion para obtener los productos asociados a una categoria
+ * Se debe enviar el id de la categoria
+*/
+const getProductByCategory = async(id) => {
+    const res = await fetch(`${urlDev}/category/${id}`);
+    const data = await res.json();
+    if(res.status === 404){
+        console.log(data.message);
+        return;
+    };
+    return data;
+};
